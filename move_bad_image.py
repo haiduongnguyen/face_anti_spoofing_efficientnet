b70fce5533002b19069c3679a3ac59151efefa0f
haiduong667799@gmail.com <haiduong667799@gmail.com>
2021-05-14 20:48:51 +0700

    update move_bad_image

--- move_bad_image.py
+++ move_bad_image.py
@@ -2,21 +2,29 @@ import os
 import shutil
 from tqdm import tqdm
 
-file1 = open('bad_images.txt', 'r')
-Lines = file1.readlines()
- 
+a_file = open("bad_images.txt", "r")
+
+list_of_paths = []
+for line in a_file:
+  stripped_line = line.strip()
+  list_of_paths.append(stripped_line)
+
+a_file.close()
+
+
 count = 0
-# Strips the newline character
-for line in tqdm(Lines):
+for line in tqdm(list_of_paths):
+    # line = line[:-3]
     count += 1
     # print("Line{}: {}".format(count, line.strip()))
     partitions = line.split("/")
-    new_destination = '/home/duongnh/liveness_detection_efficienetb4_20210515_ver02/face_anti_spoofing_efficientnet/bad_image/' + partitions[-1]
-    shutil.move(line, new_destination)
+    # new_destination = '/home/duongnh/liveness_detection_efficienetb4_20210515_ver02/face_anti_spoofing_efficientnet/bad_image/' + partitions[-1]
+    # shutil.move(line, new_destination)
+    print(line)
 
+    
 print(count)
 print("complete move bad images to new folder")
-
 
 # 3 ways to move a file to new destination
 
