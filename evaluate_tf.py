e5b00cfadebacdb8541dd2fd19458b40cfa8baca
haiduong667799@gmail.com <haiduong667799@gmail.com>
2021-06-10 21:53:51 +0700

    eval bo ver 5

--- evaluate_tf.py
+++ evaluate_tf.py
@@ -205,9 +205,16 @@ if __name__ == '__main__':
     # for index in index_checkpoint:
     #     eval(model_path, index, result_folder) 
 
-    model_name = 'new_b0_ver0'
+    # model_name = 'new_b0_ver0'
+    # result_folder = work_place + '/result_' + model_name 
+    # model_path = result_folder + '/train/checkpoint'
+    # index_checkpoint = ['cp_01.h5', 'cp_02.h5' ]
+    # for index in index_checkpoint:
+    #     eval(model_path, index, result_folder) 
+
+    model_name = 'new_b0_ver5'
     result_folder = work_place + '/result_' + model_name 
     model_path = result_folder + '/train/checkpoint'
-    index_checkpoint = ['cp_01.h5', 'cp_02.h5' ]
+    index_checkpoint = ['cp_01.h5', 'cp_02.h5', 'cp_03.h5', 'cp_05.h5' ]
     for index in index_checkpoint:
         eval(model_path, index, result_folder) 
