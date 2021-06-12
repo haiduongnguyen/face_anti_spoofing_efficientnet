e449e4a0f91a7f875a545297a54e8f9770b9859f
haiduong667799@gmail.com <haiduong667799@gmail.com>
2021-06-12 18:43:12 +0700

    clean code

--- train.py
+++ train.py
@@ -202,14 +202,14 @@ class config_to_train():
 
 if __name__ == '__main__':
 
-    b0_ver_1 = config_to_train(model_name='b0_ver_1', build_model=build_b0_gap(), INIT_LR=1e-4 )
+    b0_ver_1 = config_to_train(model_name='b0_ver_1', build_model=build_b0_gap, INIT_LR=1e-4 )
     checkpoint_dir, result_folder = b0_ver_1.train()
     b0_ver_1.eval(checkpoint_dir, result_folder)
 
-    b0_ver_2 = config_to_train(model_name='b0_ver_2', build_model=build_b0_gap(), INIT_LR=3e-4 )
+    b0_ver_2 = config_to_train(model_name='b0_ver_2', build_model=build_b0_gap, INIT_LR=3e-4 )
     checkpoint_dir, result_folder = b0_ver_2.train()
     b0_ver_2.eval(checkpoint_dir, result_folder)
 
-    # b0_ver_3 = config_to_train(model_name='b0_ver_3', build_model=build_b0_fully_connected(), INIT_LR=1e-4 )
+    # b0_ver_3 = config_to_train(model_name='b0_ver_3', build_model=build_b0_fully_connected, INIT_LR=1e-4 )
     # checkpoint_dir, result_folder = b0_ver_3.train()
     # b0_ver_3.eval(checkpoint_dir, result_folder)
