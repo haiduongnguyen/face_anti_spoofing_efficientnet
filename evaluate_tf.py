960215e20b9a7d72d364c8459cd8e80ddf931631
haiduong667799@gmail.com <haiduong667799@gmail.com>
2021-06-13 09:37:07 +0700

    eval b0 ver 3

--- evaluate_tf.py
+++ evaluate_tf.py
@@ -228,7 +228,7 @@ if __name__ == '__main__':
     #     eval(model_path, index, result_folder) 
 
     model_name = 'b0_ver_3'
-    result_folder = result_all_model + '/result_' + model_name 
+    result_folder = result_all_model + '/' + model_name 
     model_path = result_folder + '/train/checkpoint'
     index_checkpoint = ['cp_01.h5', 'cp_03.h5', 'cp_05.h5', 'cp_06.h5', 'cp_07.h5', 'cp_08.h5', 'cp_011.h5' ]
     for index in index_checkpoint:
